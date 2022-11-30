818d34efd81bba59bafae1572331df5fe15f0244
Akash <halder.akash2017@gmail.com>
2022-11-30 21:17:36 +0530

    First Commit

--- SubsetSum.cpp
+++ SubsetSum.cpp
@@ -31,13 +31,7 @@ void merge_lists(vector<int> &l, vector<int> v, int x)
             l.push_back(element);
     }
     sort(l.begin(), l.end());
-    //     l.push_back(l[i] + x);
-    // set<int> s;
-    // for(int i = 0; i < l.size(); i++)
-    //     s.insert(l[i]);
-    // l.clear();
-    // for(auto it = s.begin(); it != s.end(); it++)
-    //     l.push_back(*it);
+    
 }
 
 
@@ -68,14 +62,14 @@ int approx_subset_sum(vector<int> v, int target, float e)
     int n = v.size();
     vector<int> l;
     l = {0};
+    cout << "Approx subset sum" << endl;
     for(int i = 0; i < n; i++)
     {
         int x = v[i];
         merge_lists(l, v, x);
         float delta = e / (float)(2 * n);
         trim(l, delta);
-        cout << "delta is : " << delta << endl;
-        // trim(l, 0.05);
+        
         for(int i = l.size() - 1; i >= 0; i--)
         {
             if(l[i] > target)
@@ -105,6 +99,7 @@ int exact_subset_sum(vector<int> v, int target)
     int n = v.size();
     vector<int> l;
     l = {0};
+    cout << "Exact subset sum" << endl;
     for(int i = 0; i < n; i++)
     {
         int x = v[i];
@@ -122,8 +117,7 @@ int exact_subset_sum(vector<int> v, int target)
     }
     cout << l[l.size() - 1];
     cout << endl << endl;
-    return l[l.size() - 1];
-    
+    return l[l.size() - 1];    
 }
 
 
@@ -131,14 +125,17 @@ int exact_subset_sum(vector<int> v, int target)
 
 int main() {
 	// your code goes here
-    vector<int> v1, v2, v3, v4;
+    vector<int> v1, v2, v3, v4, v5;
     v1 = {104, 102, 201, 101};
     generateRandom(v2, 10);
-    generateRandom(v3, 50);
-    generateRandom(v4, 100);
+    generateRandom(v3, 20);
+    generateRandom(v4, 50);
+    generateRandom(v5, 100);
     int target;
     cout << "Enter target value" << endl;
     cin >> target;
+
+
     clock_t texact1;
     texact1 = clock();
     int exact1 = exact_subset_sum(v1, target);
@@ -149,12 +146,90 @@ int main() {
     tapprox1 = clock() - tapprox1;
     cout << "Exact time needed " << ((float)texact1)/CLOCKS_PER_SEC << endl;
     cout << "Approx time needed " << ((float)tapprox1)/CLOCKS_PER_SEC << endl;
-    // int exact2 = exact_subset_sum(v2, target);
-    // int approx2 = approx_subset_sum(v2, target, 0.40);
-    // int exact3 = exact_subset_sum(v3, target);
-    // int approx3 = approx_subset_sum(v3, target, 0.40);
-    // int exact4 = exact_subset_sum(v4, target);
-    // int approx4 = approx_subset_sum(v4, target, 0.40);
+    cout << endl << endl << endl;
+
+    clock_t texact2;
+    texact2 = clock();
+    int exact2 = exact_subset_sum(v2, target);
+    texact2 = clock() - texact2;
+    clock_t tapprox2;
+    tapprox2 = clock();
+    int approx2 = approx_subset_sum(v2, target, 0.40);
+    tapprox2 = clock() - tapprox2;
+    cout << "Exact time needed " << ((float)texact2)/CLOCKS_PER_SEC << endl;
+    cout << "Approx time needed " << ((float)tapprox2)/CLOCKS_PER_SEC << endl;
+
+
+    clock_t texact3;
+    texact3 = clock();
+    int exact3 = exact_subset_sum(v3, target);
+    texact3 = clock() - texact3;
+    clock_t tapprox3;
+    tapprox3 = clock();
+    int approx3 = approx_subset_sum(v3, target, 0.40);
+    tapprox3 = clock() - tapprox3;
+    cout << "Exact time needed " << ((float)texact3)/CLOCKS_PER_SEC << endl;
+    cout << "Approx time needed " << ((float)tapprox3)/CLOCKS_PER_SEC << endl;
     
+    
+    
+
+
+    clock_t texact4;
+    texact4 = clock();
+    int exact4 = exact_subset_sum(v4, target);
+    texact3 = clock() - texact4;
+    clock_t tapprox4;
+    tapprox4 = clock();
+    int approx4 = approx_subset_sum(v4, target, 0.40);
+    tapprox3 = clock() - tapprox4;
+    cout << "Exact time needed " << ((float)texact4)/CLOCKS_PER_SEC << endl;
+    cout << "Approx time needed " << ((float)tapprox4)/CLOCKS_PER_SEC << endl;
+    
+
+
+
+    clock_t texact5;
+    texact5 = clock();
+    int exact5 = exact_subset_sum(v5, target);
+    texact5 = clock() - texact5;
+    clock_t tapprox5;
+    tapprox5 = clock();
+    int approx5 = approx_subset_sum(v5, target, 0.40);
+    tapprox3 = clock() - tapprox5;
+    cout << "Exact time needed " << ((float)texact5)/CLOCKS_PER_SEC << endl;
+    cout << "Approx time needed " << ((float)tapprox5)/CLOCKS_PER_SEC << endl;
+
+    
+    
+    
+    
+    vector<float> y_exact, y_approx;
+    y_exact = {((float)texact1)/CLOCKS_PER_SEC, 
+                ((float)texact2)/CLOCKS_PER_SEC,
+                ((float)texact3)/CLOCKS_PER_SEC,
+                ((float)texact4)/CLOCKS_PER_SEC,
+                ((float)texact5)/CLOCKS_PER_SEC};
+    y_approx = {((float)tapprox1)/CLOCKS_PER_SEC,
+                ((float)tapprox2)/CLOCKS_PER_SEC,
+                ((float)tapprox3)/CLOCKS_PER_SEC,
+                ((float)tapprox4)/CLOCKS_PER_SEC,
+                ((float)tapprox5)/CLOCKS_PER_SEC};
+    vector<int> x;
+    x = {4, 10, 20, 50, 100};
+    
+
+    ofstream file;
+    file.open ("data.txt");
+    file << "# Dataset for exact times (index 0)\n# X   Y\n";
+    for(int i = 0; i < 5; i++)
+        file << "  " << x[i] << "   " << y_exact[i] << "\n";
+    file << "\n\n";
+    file << "# Dataset for approximate times (index 1)\n# X   Y\n";
+    for(int i = 0; i < 5; i++)
+        file << "  " << x[i] << "   " << y_approx[i] << "\n";
+    
+
+
 	return 0;
 }
